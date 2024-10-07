d6f55db12a4ff346f2a35697f6d6acc339854e64
sj_shoff <atryakhin0306#gmail.com>
2024-10-07 13:51:11 +0300

    modified:   cmd/main.go

--- cmd/main.go
+++ cmd/main.go
@@ -13,10 +13,14 @@ func main() {
 		log.Fatal(err)
 	}
 
-	s.Set("key2", "222")
+	s.Set("key1", "2024")
+	s.Set("key2", "year")
+	res1 := s.Get("key1")
+	kind1 := s.GetKind("key1")
+	res2 := s.Get("key2")
+	kind2 := s.GetKind("key2")
+	fmt.Println(s)
 
-	res1 := s.Get("key2")
-	kind := s.GetKind("key2")
-
-	fmt.Println(*res1, kind)
+	fmt.Println(*res1, kind1)
+	fmt.Println(*res2, kind2)
 }
